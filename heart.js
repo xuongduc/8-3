8ce28c17c47a9abdd8e1365c1e856ab7c3d7ee7f
xuongduc <phuongxuongduc29052005@gmail.com>
2024-03-03 16:51:02 +0700

    initial
    adding files

---
+++ heart.js
@@ -0,0 +1,37 @@
+const heart = document.getElementById("main_heart");
+const song = document.getElementById("song")
+
+heart.addEventListener("click", (clicked));
+
+
+function getIntRand(start, end){
+    return Math.round(Math.random() * end) - start + 30;
+}
+
+function clicked(){
+    heart.style.visibility = "hidden";
+    song.play();
+    for (let i = 0; i < 60; i++){
+        let sub_heart = document.createElement("span");
+        sub_heart.style.left = getIntRand(1, 1000) + 'px';
+        sub_heart.style.top = getIntRand(1, 1000) + 'px';
+        document.querySelector("body").appendChild(sub_heart)
+        setTimeout(function(){
+            sub_heart.remove();
+            heart.style.visibility = "visible";
+        }, 4000)
+    }
+    setTimeout(function(){
+        for (let i = 0; i < 60; i++){
+            let sub_heart = document.createElement("span");
+            sub_heart.style.left = getIntRand(1, 1000) + 'px';
+            sub_heart.style.top = getIntRand(1, 1000) + 'px';
+            document.querySelector("body").appendChild(sub_heart)
+            setTimeout(function(){
+                sub_heart.remove();
+                heart.style.visibility = "visible";
+            }, 4000)
+    }          
+    }, 100)
+    
+}
